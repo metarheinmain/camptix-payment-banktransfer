126d85fa4ee3fb58d83f8a4804226981868f49a5
Raphael Michel <mail@raphaelmichel.de>
2013-04-18 14:38:57 +0200

    We don't need to add extra hooks to CampTix (see Automattic/camptix#42)

--- README.md
+++ README.md
@@ -5,11 +5,7 @@ Simple WordPress plugin which adds bank transfer support to CampTix
 
 Installation
 ------------
-* Install and set up CampTix. We need some extra hooks which we requested
-  to be integrated into CampTix mainline, but for now you need this patch:
-  [mrmcd13/camptix 71aeeb9](https://github.com/mrmcd13/camptix/commit/71aeeb98a80a304e37deab8ea36f74d24d3525cf)
-
-  Up-to-date information on this issue: [Automattic/camptix #42](https://github.com/Automattic/camptix/pull/42)
+* Install and set up CampTix.
 * Install this plugin:
 ```
 $ cd my/wordpress/installation/wp-content/plugins/
--- camptix-payment-banktransfer.php
+++ camptix-payment-banktransfer.php
@@ -59,7 +59,7 @@ class CampTix_Payment_Method_Banktransfer extends CampTix_Payment_Method {
 			'bankdetails' => '',
 		), $this->get_payment_options() );
 		
-		add_action( 'camptix_payment_pending', array($this, 'payment_pending_information'), 10, 1 );
+		add_action( 'camptix_notices',  array($this, 'payment_pending_information'), 11 );
 		
 		add_action( 'camptix_init_email_templates_shortcodes', array( $this, 'init_email_templates_shortcodes' ), 9 );
 	}
@@ -81,17 +81,50 @@ class CampTix_Payment_Method_Banktransfer extends CampTix_Payment_Method {
 		return $bankdetails;
 	}
 	
-	function payment_pending_information($attendee) {
+	function payment_pending_information() {
 		global $camptix;
+
+		if ( 'edit_attendee' == get_query_var( 'tix_action' ) ) {
+			$attendee_id = intval( $_REQUEST['tix_attendee_id'] );
+			$attendee = get_post( $attendee_id );
+		} else if ( 'access_tickets' == get_query_var( 'tix_action' ) ) {
+			$access_token = $_REQUEST['tix_access_token'];
+			$is_refundable = false;
+
+			// Let's get one attendee
+			$attendees = get_posts( array(
+				'posts_per_page' => 1,
+				'post_type' => 'tix_attendee',
+				'post_status' => array( 'publish', 'pending' ),
+				'meta_query' => array(
+					array(
+						'key' => 'tix_access_token',
+						'value' => $access_token,
+						'compare' => '=',
+						'type' => 'CHAR',
+					),
+				),
+				'cache_results' => false,
+			) );
+			$attendee = $attendees[0];
+			$attendee_id = $attendee->ID;
+		} else {
+			return;
+		}
+
+		if ( $attendee->post_status != 'pending' )
+			return;
 		
-		if(get_post_meta( $attendee->ID, 'tix_payment_method', true ) == $this->id){
+		if ( get_post_meta( $attendee_id, 'tix_payment_method', true ) == $this->id ){
 			
-			$price = $camptix->append_currency( get_post_meta( $attendee->ID, 'tix_order_total', true ) );
+			$price = $camptix->append_currency( get_post_meta( $attendee_id, 'tix_order_total', true ) );
 			$bankdetails = nl2br($this->options['bankdetails']);
-			$bankdetails .= sprintf( "<br />" . __( "Reference: <strong>%s</strong>", 'camptixpaymentbanktransfer' ), get_post_meta( $attendee->ID, 'tix_banktransfer_token', true ));
+			$bankdetails .= sprintf( "<br />" . __( "Reference: <strong>%s</strong>", 'camptixpaymentbanktransfer' ), get_post_meta( $attendee_id, 'tix_banktransfer_token', true ));
 			$bankdetails .= sprintf( "<br />" . __( "Amount: %s", 'camptixpaymentbanktransfer' ),  $price );
 			
-			$camptix->notice( 'raw:' . __( "Please transfer your money to the following bank account:", 'camptixpaymentbanktransfer' ) . "<br />" . $bankdetails );
+			echo '<p class="tix-notice">';
+			echo  __( "Please transfer your money to the following bank account:", 'camptixpaymentbanktransfer' ) . "<br />" . $bankdetails;
+			echo '</p>';
 		}
 	}
 
